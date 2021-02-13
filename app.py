26f93fbd02dd49960d888241de2d47dc1eefd59b
mstanton <mrstanton81@gmail.com>
2021-02-13 14:18:11 -0600

    initial

---
+++ app.py
@@ -0,0 +1,42 @@
+# RPi RC Adventures - Let's GOOOOOOooooo!
+# DATE: 2021-02-11
+# DESC: Flask app for controlling an RC car.
+
+from flask import Flask, render_template, Response, request
+from camera import VideoCamera
+import time
+import threading
+import os
+import datetime
+
+pi_camera = VideoCamera(flip=True) # flip pi camera if upside down.
+
+app = Flask(__name__, static_folder='static')
+
+@app.route("/")
+def index():
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        'title' : "RPi Adventure Time with Python &amp; Flask",
+        'time' : timeString
+    }
+    
+    return render_template('index.html', **templateData)
+
+def gen(camera):
+    # get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+
---
+++ camera.py
@@ -0,0 +1,28 @@
+# RPi Camera
+# DATE: 2021-02-11
+# DESCRIPTION: VideoCamera Class - Access the RPi Camera for streaming
+
+import cv2
+from imutils.video.pivideostream import PiVideoStream
+import imutils
+import time
+import numpy as np
+
+class VideoCamera(object):
+    def __init__(self, flip = False ):
+        self.vs = PiVideoStream().start()
+        self.flip = flip
+        time.sleep(2.0)
+        
+    def __del__(self):
+        self.vs.stop
+    
+    def flip_if_needed(self, frame):
+        if self.flip:
+            return np.flip(frame, 0)
+        return frame
+    
+    def get_frame(self):
+        frame = self.flip_if_needed(self.vs.read())
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
